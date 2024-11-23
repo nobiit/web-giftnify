9c7874724fb7c6879f04a204dd5b678ab99e513a
Nobi <nobi@nobidev.com>
2024-11-24 01:53:23 +0700

    Add header, navigation, and logo styles to the project

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './Home.module.css';
+import {Header} from './components';
 import {fallbackLng, languages} from '@i18n/settings';
 import {useTranslation} from '@i18n';
 
@@ -9,10 +11,11 @@ export default async function Page({params}: {
 }) {
   let {lng} = await params;
   if (languages.indexOf(lng) < 0) lng = fallbackLng;
-  const {t} = await useTranslation(lng);
+  const i18n = await useTranslation(lng);
 
   return (
-    <main>
+    <main className={styles.landingPage}>
+      <Header i18n={i18n}/>
     </main>
   );
 };
---
+++ app/constants/index.ts
@@ -0,0 +1,18 @@
+import {NavItem} from '@types';
+
+export const APP_NAME = 'Giftnify';
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: 'create_game',
+    href: '#create',
+  },
+  {
+    label: 'pricing',
+    href: '#pricing',
+  },
+  {
+    label: 'contact',
+    href: '#contact',
+  },
+];
