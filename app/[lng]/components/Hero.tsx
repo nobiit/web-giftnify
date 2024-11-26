f3b8685d8b38cab46ba265d61d0c53105ea0b105
Nobi <nobi@nobidev.com>
2024-11-26 08:42:10 +0700

    Add NextIcon component and update devDependencies in package.json

--- app/[lng]/components/Hero.tsx
+++ app/[lng]/components/Hero.tsx
@@ -4,6 +4,7 @@ import {I18n, i18nWithNs} from '@i18n';
 import bg1 from '@assets/images/bg-1.png';
 import bg2 from '@assets/images/bg-2.png';
 import bg3 from '@assets/images/bg-3.png';
+import NextIcon from '@assets/icons/next.svg';
 import {Carousel} from './Carousel';
 
 export const Hero: React.FC<{ i18n: I18n }> = async ({i18n}) => {
@@ -13,7 +14,11 @@ export const Hero: React.FC<{ i18n: I18n }> = async ({i18n}) => {
     <section className={styles.hero}>
       <Carousel src={bg1} items={[bg1, bg2, bg3]} alt="Hero Carousel" className={styles.heroBackground}/>
       <h1 className={styles.heroTitle}>{t('title')}</h1>
-      <button className={styles.ctaButton}>{t('button')}</button>
+      <button className={styles.ctaButton}>
+        {t('button')}
+        &nbsp;
+        <NextIcon/>
+      </button>
     </section>
   );
 };
