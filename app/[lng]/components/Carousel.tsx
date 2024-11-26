8cfe0fa7e43fa446297cd82cd5f35f6b35237761
Nobi <nobi@nobidev.com>
2024-11-26 16:40:42 +0700

    Adjust styling to ensure images fill height on smaller screens

--- app/[lng]/components/Carousel.tsx
+++ app/[lng]/components/Carousel.tsx
@@ -4,7 +4,6 @@ import React, {ComponentProps, useCallback, useEffect, useRef} from 'react';
 import Image, {StaticImageData} from 'next/image';
 import useCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
-import {useWindowSize} from '@hooks/useWindowSize';
 
 import './Carousel.css';
 
@@ -13,7 +12,6 @@ interface Props extends ComponentProps<typeof Image> {
 }
 
 export const Carousel: React.FC<Props> = ({className, items, src: defaultSrc, ...props}) => {
-  const winSize = useWindowSize();
   const ref = useRef<HTMLDivElement>(null);
   const [setRef, api] = useCarousel({loop: true}, [Autoplay()]);
 
@@ -27,25 +25,9 @@ export const Carousel: React.FC<Props> = ({className, items, src: defaultSrc, ..
 
   useEffect(() => {
     if (ref && ref.current) {
-      // setRef(ref);
+      setRef(ref.current);
     }
   }, [ref]);
-
-  useEffect(() => {
-    if (ref && ref.current) {
-      const rootEl = ref.current;
-      const containerEl = rootEl.querySelector('.embla__container');
-      if (!containerEl) {
-        return;
-      }
-      const className = 'fill-height';
-      if (containerEl.clientHeight < winSize.height) {
-        containerEl.classList.add(className);
-      } else {
-        containerEl.classList.remove(className);
-      }
-    }
-  }, [ref, winSize]);
 
   return <div className={`embla ${className}`} ref={ref}>
     <div className="embla__container">
