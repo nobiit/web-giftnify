88b8c4744a1d1daf3353fa7d89d7d2bced2e16b5
Nobi <nobi@nobidev.com>
2024-11-26 10:52:53 +0700

    Refactor constant export for NAV_ITEMS to a separate file

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,18 +1,3 @@
-import {NavItem} from '@types';
+export {NAV_ITEMS} from './nav';
 
 export const APP_NAME = 'Giftnify';
-
-export const NAV_ITEMS: NavItem[] = [
-  {
-    label: 'create_game',
-    href: '#create',
-  },
-  {
-    label: 'pricing',
-    href: '#pricing',
-  },
-  {
-    label: 'contact',
-    href: '#contact',
-  },
-];
---
+++ app/constants/nav.ts
@@ -0,0 +1,16 @@
+import {NavItem} from '@types';
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: 'create_game',
+    href: '#create',
+  },
+  {
+    label: 'pricing',
+    href: '#pricing',
+  },
+  {
+    label: 'contact',
+    href: '#contact',
+  },
+];
