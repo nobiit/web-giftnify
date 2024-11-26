13afbceef6e0f968c6526f099a2d525b6e0f5002
Nobi <nobi@nobidev.com>
2024-11-26 10:39:03 +0700

    Rename Hero module and component to Landing module and component

--- app/[lng]/components/Landing.tsx
+++ app/[lng]/components/Landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Hero.module.css';
+import styles from './Landing.module.css';
 import {I18n, i18nWithNs} from '@i18n';
 import bg1 from '@assets/images/bg-1.png';
 import bg2 from '@assets/images/bg-2.png';
@@ -7,14 +7,14 @@ import bg3 from '@assets/images/bg-3.png';
 import NextIcon from '@assets/icons/next.svg';
 import {Carousel} from './Carousel';
 
-export const Hero: React.FC<{ i18n: I18n }> = async ({i18n}) => {
-  const {t} = await i18nWithNs(i18n, 'hero');
+export const Landing: React.FC<{ i18n: I18n }> = async ({i18n}) => {
+  const {t} = await i18nWithNs(i18n, 'landing');
 
   return (
-    <section className={styles.hero}>
-      <Carousel src={bg1} items={[bg1, bg2, bg3]} alt="Hero Carousel" className={styles.heroBackground}/>
+    <section className={styles.landing}>
+      <Carousel src={bg1} items={[bg1, bg2, bg3]} alt="Landing Carousel" className={styles.landingBackground}/>
       <div className={styles.overlay}>
-        <h1 className={styles.heroTitle}>{t('title')}</h1>
+        <h1 className={styles.landingTitle}>{t('title')}</h1>
         <button className={styles.ctaButton}>
           {t('button')}
           &nbsp;
--- app/[lng]/components/index.ts
+++ app/[lng]/components/index.ts
@@ -1,3 +1,3 @@
 export * from './Navigation';
 export * from './Header';
-export * from './Hero';
+export * from './Landing';
