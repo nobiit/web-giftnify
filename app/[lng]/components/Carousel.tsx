6ee2ddbe27066ff374440ae501b671e9213b63af
Nobi <nobi@nobidev.com>
2024-11-26 16:22:25 +0700

    Add dynamic resizing functionality to Carousel component based on window size

--- app/[lng]/components/Carousel.tsx
+++ app/[lng]/components/Carousel.tsx
@@ -1,8 +1,10 @@
 'use client';
-import React, {ComponentProps, useCallback} from 'react';
+
+import React, {ComponentProps, useCallback, useEffect, useRef} from 'react';
 import Image, {StaticImageData} from 'next/image';
 import useCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
+import {useWindowSize} from '@hooks/useWindowSize';
 
 import './Carousel.css';
 
@@ -11,7 +13,9 @@ interface Props extends ComponentProps<typeof Image> {
 }
 
 export const Carousel: React.FC<Props> = ({className, items, src: defaultSrc, ...props}) => {
-  const [ref, api] = useCarousel({loop: true}, [Autoplay()]);
+  const winSize = useWindowSize();
+  const ref = useRef<HTMLDivElement>(null);
+  const [setRef, api] = useCarousel({loop: true}, [Autoplay()]);
 
   const scrollPrev = useCallback(() => {
     if (api) api.scrollPrev();
@@ -20,6 +24,28 @@ export const Carousel: React.FC<Props> = ({className, items, src: defaultSrc, ..
   const scrollNext = useCallback(() => {
     if (api) api.scrollNext();
   }, [api]);
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      // setRef(ref);
+    }
+  }, [ref]);
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      const rootEl = ref.current;
+      const containerEl = rootEl.querySelector('.embla__container');
+      if (!containerEl) {
+        return;
+      }
+      const className = 'fill-height';
+      if (containerEl.clientHeight < winSize.height) {
+        containerEl.classList.add(className);
+      } else {
+        containerEl.classList.remove(className);
+      }
+    }
+  }, [ref, winSize]);
 
   return <div className={`embla ${className}`} ref={ref}>
     <div className="embla__container">
